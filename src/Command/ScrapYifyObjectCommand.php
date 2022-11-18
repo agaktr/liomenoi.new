a6201704452420ee43bc384a791bbde5da4bf767
agaktr <katradis.agamemnon@gmail.com>
2022-11-18 22:03:00 +0200

    1st

--- src/Command/ScrapYifyObjectCommand.php
+++ src/Command/ScrapYifyObjectCommand.php
@@ -50,7 +50,7 @@ class ScrapYifyObjectCommand extends Command
 
         $io->title('Starting to scrap YIFY');
 
-        $objects = $this->em->getRepository(YifyObject::class)->findBy(['fetched' => false],[ 'id' => 'ASC'], 2,0);
+        $objects = $this->em->getRepository(YifyObject::class)->findBy(['fetched' => false],[ 'id' => 'ASC'], 10,0);
 
         $objectsMap = [];
         foreach ($objects as $object) {
