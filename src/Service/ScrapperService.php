91fc9d8bb0c1f387efb59bd16b1e907a102f810c
agaktr <katradis.agamemnon@gmail.com>
2022-11-18 22:15:29 +0200

    1st

--- src/Service/ScrapperService.php
+++ src/Service/ScrapperService.php
@@ -216,6 +216,8 @@ class ScrapperService
     {
         $finder = new DomXPath($dom);
         $elements = $finder->query("//*[contains(concat(' ', normalize-space(@title), ' '), ' $titleName ')]");
+
+        var_dump($elements);
         if ($single) {
             $newDom = new DomDocument();
             $newDom->appendChild($newDom->importNode($elements->item(0), true));
