36ef7eaf397e88d6714643934dcce265ba860ec0
agaktr <katradis.agamemnon@gmail.com>
2022-11-18 23:05:02 +0200

    1st

--- src/Service/ScrapperService.php
+++ src/Service/ScrapperService.php
@@ -189,13 +189,12 @@ class ScrapperService
 
                 $magnetClassname="magnet-download";
                 $magnetElement = $tmpElFinder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $magnetClassname ')]")->item(0);
-var_dump($magnetElement);
-var_dump($magnetElement->getAttribute('href'));
+
                 $this->scrappedContent[$id]['magnet'][$k]['quality'] = trim($qualityElement->nodeValue);
                 $this->scrappedContent[$id]['magnet'][$k]['type'] = trim($qualitySizeElements->item(0)->nodeValue);
                 $this->scrappedContent[$id]['magnet'][$k]['size'] = trim($qualitySizeElements->item(1)->nodeValue);
                 $this->scrappedContent[$id]['magnet'][$k]['magnet'] = $magnetElement->getAttribute('href');
-
+                var_dump($this->scrappedContent[$id]);
             }
         }
 
