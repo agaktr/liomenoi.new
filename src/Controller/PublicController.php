eb60f8446eaae56d9ca60045d7f357a15a8bc615
agaktr <katradis.agamemnon@gmail.com>
2022-11-21 14:38:01 +0200

    1st

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Controller\Apto\AptoAbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Tmdb\Client;
 
 
 class PublicController extends AptoAbstractController
@@ -14,6 +15,9 @@ class PublicController extends AptoAbstractController
      */
     public function index(): Response
     {
+
+        $tmdbClient = new Client();
+        var_dump($tmdbClient->getMoviesApi()->getMovie(550));
 
         return $this->render('public/home.html.twig', [
         ]);
