d94aaa86dd9b2a491c35db3366a96dee37920e92
agaktr <katradis.agamemnon@gmail.com>
2022-11-22 20:44:08 +0200

    1st

--- src/Command/ScrapCommand.php
+++ src/Command/ScrapCommand.php
@@ -217,6 +217,34 @@ class ScrapCommand extends Command
         return Command::SUCCESS;
     }
 
+    private function slugify($text, string $divider = '-')
+    {
+        // replace non letter or digits by divider
+        $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        // trim
+        $text = trim($text, $divider);
+
+        // remove duplicate divider
+        $text = preg_replace('~-+~', $divider, $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
+    }
+
+
     private function handleMovie(int $objectId, array $movieData,$io)
     {
         /** @var Movie $movie */
@@ -353,6 +381,9 @@ class ScrapCommand extends Command
 
             $movie->addActor($this->actorsLocalArray[$actorModel->getId()]);
         }
+
+        //set slug
+        $movie->setSlug($this->slugify($movie->getOriginalTitle().'-'.$movie->getYear()));
 
         $movie->setFetched(true);
 
