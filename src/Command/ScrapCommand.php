8e88a2fe915e68b999c051310bdfe8df50e9b3ef
agaktr <katradis.agamemnon@gmail.com>
2022-11-22 21:49:56 +0200

    1st

--- src/Command/ScrapCommand.php
+++ src/Command/ScrapCommand.php
@@ -94,7 +94,7 @@ class ScrapCommand extends Command
 
             //init variables
             $hasMore = true;
-            $pagesNo = 1;
+            $pagesNo = 2;
             $doing = 'Movie';
 
             switch ($doing) {
