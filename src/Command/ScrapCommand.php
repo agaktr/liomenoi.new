9a8f52e300480b5dbe3efdf9a73e866fac16f87a
agaktr <katradis.agamemnon@gmail.com>
2022-11-22 20:59:16 +0200

    1st

--- src/Command/ScrapCommand.php
+++ src/Command/ScrapCommand.php
@@ -252,7 +252,7 @@ class ScrapCommand extends Command
         $objectKey =$movieData[ 'data' ]->getName().'-'.$movieData[ 'data' ]->getYear();
 
         if(!isset($this->objectsLocalArray[$objectKey])){
-            $io->info(': Creating new Object ');
+            $io->warning(': Creating new Object ');
             $movie = new Movie();
             $this->em->persist($movie);
             $this->objectsLocalArray[$objectKey] = $movie;
