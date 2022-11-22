fe5c2278afe759197b8da39ee1f19bbe0672a81d
agaktr <katradis.agamemnon@gmail.com>
2022-11-22 15:26:57 +0200

    1st

--- src/Entity/Provider.php
+++ src/Entity/Provider.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ProviderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -41,6 +43,16 @@ class Provider
      * @ORM\Column(type="string", length=180)
      */
     private $pageQueryString;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Scrap::class, mappedBy="provider")
+     */
+    private $scraps;
+
+    public function __construct()
+    {
+        $this->scraps = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -103,6 +115,36 @@ class Provider
     public function setPageQueryString(string $pageQueryString): self
     {
         $this->pageQueryString = $pageQueryString;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Scrap>
+     */
+    public function getScraps(): Collection
+    {
+        return $this->scraps;
+    }
+
+    public function addScrap(Scrap $scrap): self
+    {
+        if (!$this->scraps->contains($scrap)) {
+            $this->scraps[] = $scrap;
+            $scrap->setProvider($this);
+        }
+
+        return $this;
+    }
+
+    public function removeScrap(Scrap $scrap): self
+    {
+        if ($this->scraps->removeElement($scrap)) {
+            // set the owning side to null (unless already changed)
+            if ($scrap->getProvider() === $this) {
+                $scrap->setProvider(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Scrap.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ScrapRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ScrapRepository::class)
+ */
+class Scrap
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=180)
+     */
+    private $year;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updated;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $valid;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Provider::class, inversedBy="scraps")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $provider;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getYear(): ?string
+    {
+        return $this->year;
+    }
+
+    public function setYear(string $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getUpdated(): ?\DateTimeInterface
+    {
+        return $this->updated;
+    }
+
+    public function setUpdated(\DateTimeInterface $updated): self
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+
+    public function isValid(): ?bool
+    {
+        return $this->valid;
+    }
+
+    public function setValid(?bool $valid): self
+    {
+        $this->valid = $valid;
+
+        return $this;
+    }
+
+    public function getProvider(): ?Provider
+    {
+        return $this->provider;
+    }
+
+    public function setProvider(?Provider $provider): self
+    {
+        $this->provider = $provider;
+
+        return $this;
+    }
+}
