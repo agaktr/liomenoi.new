8addc803733667d6088d4bdb64a60698235bdfb9
agaktr <katradis.agamemnon@gmail.com>
2022-11-23 18:47:30 +0200

    1st

--- src/Service/ScrapperService.php
+++ src/Service/ScrapperService.php
@@ -251,6 +251,7 @@ class ScrapperService
 
                 //calculate size
                 $size = 0;
+                var_dump($torrent->info);
                 foreach ($torrent->info['files'] as $file) {
                     $size += $file['length'];
                 }
