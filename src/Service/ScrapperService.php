fd5c501ac07d4940a0a416973022b6eb07fc404f
agaktr <katradis.agamemnon@gmail.com>
2022-11-29 14:11:47 +0200

    1st

--- src/Service/ScrapperService.php
+++ src/Service/ScrapperService.php
@@ -148,8 +148,8 @@ class ScrapperService
         } while($index > 0);
 
         foreach($multiCurl as $id => $ch) {
-            var_dump(4);
-            var_dump($id);
+            var_dump(curl_getinfo($ch));
+            var_dump(curl_multi_getcontent($ch));
             $this->urlContent[$id]['body'] = curl_multi_getcontent($ch);
             $this->urlContent[$id]['header'] = curl_getinfo($ch);
             curl_multi_remove_handle($mh, $ch);
